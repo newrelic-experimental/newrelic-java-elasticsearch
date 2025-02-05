77adca641e0d96aad1d9bc02b09a7928699a319c
gsidhwani_nr <gsidhwani@newrelic.com>
2025-02-05 16:36:34 +0530

    feat: elasticrestclient710

---
+++ esrestclient-7.10/src/main/java/com/newrelic/instrumentation/labs/esrestclient/ESQueryConverter.java
@@ -0,0 +1,25 @@
+package com.newrelic.instrumentation.labs.esrestclient;
+
+import com.newrelic.api.agent.QueryConverter;
+
+public class ESQueryConverter implements QueryConverter<String> {
+
+	@Override
+	public String toRawQueryString(String rawQuery) {
+		return rawQuery;
+	}
+
+	@Override
+	/**
+	 * Always obfuscate the id
+	 */
+	public String toObfuscatedQueryString(String rawQuery) {
+		int index = rawQuery.lastIndexOf('/');
+		if(index < 1) {
+			return rawQuery;
+		}
+		
+		return rawQuery.substring(0, index) + "/?";
+	}
+
+}
---
+++ esrestclient-7.10/src/main/java/com/newrelic/instrumentation/labs/esrestclient/Utils.java
@@ -0,0 +1,58 @@
+package com.newrelic.instrumentation.labs.esrestclient;
+
+
+import java.util.HashMap;
+import java.util.Map;
+import org.elasticsearch.client.Request;
+import com.newrelic.api.agent.DatastoreParameters;
+import com.newrelic.api.agent.NewRelic;
+
+public class Utils {
+
+  public static void addAttribute(Map<String, Object> attributes, String key, Object value) {
+    if (key != null && !key.isEmpty() && value != null) {
+      attributes.put(key, value);
+    }
+  }
+
+
+
+  public static void addRequestAttributes(Map<String, Object> attributes, Object request) {
+    if (request instanceof Request) {
+      Request req = (Request) request;
+      // Add attributes directly from the Request object
+      addAttribute(attributes, "Request-Method", req.getMethod());
+      addAttribute(attributes, "Request-Endpoint", req.getEndpoint());
+
+
+      // Add parameters if present
+      if (!req.getParameters().isEmpty()) {
+        addAttribute(attributes, "Request-Parameters", req.getParameters().toString());
+      }
+
+      // Add entity if present
+      if (req.getEntity() != null) {
+        addAttribute(attributes, "Request-Entity", req.getEntity().toString());
+      }
+
+      // Add options
+      addAttribute(attributes, "Request-Options", req.getOptions().toString());
+    }
+
+    // System.out.println(attributes);
+  }
+
+
+  public static DatastoreParameters createDatastoreParams(String operation, String index,
+      String query) {
+    return DatastoreParameters.product("Elasticsearch").collection(index).operation(operation)
+        .instance("localhost", 9200) // Adjust host and port as necessary
+        .noDatabaseName().slowQuery(query, new ESQueryConverter()).build();
+  }
+
+  public static void logRequestAttributes(Object request) {
+    Map<String, Object> attributes = new HashMap<>();
+    addRequestAttributes(attributes, request);
+    NewRelic.getAgent().getTracedMethod().addCustomAttributes(attributes);
+  }
+}
